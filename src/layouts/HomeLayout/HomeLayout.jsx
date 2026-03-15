54da332f326754fae7a402ef597610b8f1f67a10
tanvir <mstanvirislam05@gmail.com>
2026-03-15 22:31:22 +0600

    remove unused code and add layout componet and basic nav

---
+++ src/layouts/HomeLayout/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../../components/Navbar/Nav';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <Nav/>
+        </div>
+    );
+};
+
+export default HomeLayout;
